69e483df79967cf970f270401866ffaa596ec4bd
husain <h.y.patanwala@gmail.com>
2026-03-03 19:22:02 -0800

    fill out auto connect script functionality

--- Assets/Scripts/AutoConnect.cs
+++ Assets/Scripts/AutoConnect.cs
@@ -1,16 +1,29 @@
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 public class AutoConnect : MonoBehaviour
 {
+
+    [Header("Set this before building")]
+    public bool isHost = true;
+
+    [Header("Client settings")]
+    public string hostip = "";
+    public ushort port = 7777;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+        if (isHost)
+        {
+            NetworkManager.Singleton.StartHost();
+        }
+        else
+        {
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(hostip, port);
+            NetworkManager.Singleton.StartClient();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
